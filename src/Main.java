17124da34585f61b079f3660785f4c87d3ef2d4a
Mehmet Ali Öter <memoliotr.47@gmail.com>
2025-12-06 14:37:53 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Git test successful!");
-
+        System.out.println("Second commit test!");
     }
-
-
 }
