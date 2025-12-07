e8bf2ec2b4cf80370dfcc7df64491f3b19318cc2
Mehmet Ali Öter <memoliotr.47@gmail.com>
2025-12-07 20:29:40 +0300

    The first class of the project was created. Student

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private String number;
+    private String name;
+    private String surname;
+    private String department;
+
+    public Student(String number, String name, String surname, String department){
+        this.number = number;
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+    }
+}
