2e9215c83352bfdbf366fb800725d04a3f8f3650
Mehmet Ali Öter <memoliotr.47@gmail.com>
2025-12-08 16:49:42 +0300

    Created my Registration class handling student–course relations with timestamp

--- src/Registration.java
+++ src/Registration.java
@@ -1,2 +1,18 @@
-public class Registration {
+import java.time.LocalDate;
+
+public class Registration implements java.io.Serializable{
+
+    private Student student;
+    private Course course;
+    private LocalDate date;
+
+    public Registration(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+        this.date = LocalDate.now();
+    }
+
+    public Student getStudent() {return student;}
+    public Course getCourse() {return course;}
+    public LocalDate getDate() {return date;}
 }
