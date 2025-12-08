b5cf33c599271189fc59d68768f7d693a3f9adc2
Mehmet Ali Öter <memoliotr.47@gmail.com>
2025-12-08 16:30:42 +0300

    I Added Implement CourseCatalog with HashMap-based course management (add/get/remove)

--- src/CourseCatalog.java
+++ src/CourseCatalog.java
@@ -1,2 +1,30 @@
-public class CourseCatalog {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CourseCatalog implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private Map<String, Course> courses = new HashMap<>();
+
+    public void addCourse(Course course){
+        courses.put(course.getCode().toUpperCase(), course);
+    }
+    public boolean removeCourse(String code){
+        return courses.remove(code.toUpperCase()) != null;
+    }
+    public Course getCourse(String code){
+        return courses.get(code.toUpperCase());
+    }
+
+
+
+
+
+
+
+
+
+
 }
