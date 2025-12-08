ee4bd8f933ee2157e8d4e7276e93b3fe81287b04
Mehmet Ali Öter <memoliotr.47@gmail.com>
2025-12-08 16:19:37 +0300

    I added the course class with code, name and ECTS fields (Serializable)

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,18 @@
-public class Course {
+public class Course implements java.io.Serializable{
+
+    private String code;
+    private String name;
+    private int ects;
+
+    public Course(String code, String name, int ects){
+        this.code = code.toUpperCase();
+        this.name = name;
+        this.ects = ects;
+    }
+    public String getCode() {return code;}
+    public String getName() {return name;}
+    public int getEcts() {return ects;}
+
+    public void setName(String name) {this.name = name;}
+    public void setEcts(int ects) {this.ects = ects;}
 }
