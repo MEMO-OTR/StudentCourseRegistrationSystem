d99c3388d7f6f5025d1bc194548f36665e4376ec
Mehmet Ali Öter <memoliotr.47@gmail.com>
2025-12-08 16:37:07 +0300

    I added Implemented GraduateStudent class with custom tuition calculation

--- src/GraduteStudent.java
+++ src/GraduteStudent.java
@@ -1,2 +1,11 @@
-public class GraduteStudent {
+public class GraduteStudent extends Student implements java.io.Serializable {
+
+    public GraduteStudent(String number, String name, String surname, String department){
+        super(number, name, surname, department);
+    }
+
+    @Override
+    public double calculateTuition() {
+        return registrations.size() * 5.350;
+    }
 }
