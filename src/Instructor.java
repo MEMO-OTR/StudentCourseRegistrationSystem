6ef8809a43f5fdcbead23bcb3f69f0169789f3f7
Mehmet Ali Öter <memoliotr.47@gmail.com>
2025-12-08 16:42:51 +0300

    I added Implemented Instructor class with name and department fields

--- src/Instructor.java
+++ src/Instructor.java
@@ -1,2 +1,13 @@
 public class Instructor {
+
+    private String name;
+    private String department;
+
+    public Instructor(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getName() {return name;}
+    public String getDepartment() {return department;}
 }
